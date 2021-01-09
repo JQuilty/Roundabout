19fa66096449361f8753f3a16c365bf19aad4d5d
John Quilty <jquilty@gmail.com>
2021-01-08 18:08:37 -0600

    Adding first routes

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
+const connectDB = require('./config/db');
+
+//Uncomment when done to connect database
+// connectDB();
 app.get('/',(req, res) => res.send('Running'));
+
+//Routes
+
+app.use('/api/users', require('./routes/api/users'));
 
 const PORT = process.env.PORT || 8492
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
